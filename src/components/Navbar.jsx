87d6a537c9f5dd55c82bed21cf34626f1fa147c6
Shiven Goomer <shivengoomer@gmail.com>
2025-01-29 22:25:46 +0530

    Navbar Bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import { Link, NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useContext, useState } from "react";
-<<<<<<< HEAD
-=======
 import { ShopContext } from "../context/ShopContext";
 import Logo from '../myassets/sheshoplogo.png'
->>>>>>> c474a8d55155b97764a6551063140501061b5ba0
+
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
   const { setShowSearch, getCartCount } = useContext(ShopContext);
